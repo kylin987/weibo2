36aa3461b4f0456aadb2ef490cd2f67a713bae01
kylin87 <kylin87@qq.com>
2019-11-19 05:59:50 +0000

    增加CSRF验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,11 +8,25 @@ use App\Models\User;
 class UsersController extends Controller
 {
 
+    //显示用户注册页面
     public function create(){
         return view('users.create');
     }
 
+    //显示用户信息页面
     public function show(User $user){
         return view('users.show',compact('user'));
+    }
+
+
+    //存储提交的注册信息
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return;
     }
 }
